392817794b034f012850b28c8c81a9c065b7563c
Tyler Lundgren <tylerlundgren@yahoo.com>
2023-02-14 13:40:08 -0700

    Trace removal.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // External Dependencies
 import { ApolloServer } from 'apollo-server';
 import { ApolloGateway } from '@apollo/gateway';
+const { ApolloServerPluginInlineTraceDisabled } = require("apollo-server-core");
 
 export async function buildGateway(port) {
     const gateway = new ApolloGateway({});
   
     const server = new ApolloServer({
       gateway,
+      plugins: [
+        ApolloServerPluginInlineTraceDisabled(),
+      ],
     });
     const { url } = await server.listen(port);
     console.log(`Gateway running ${url}`);
